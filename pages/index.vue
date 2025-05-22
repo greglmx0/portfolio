9a364c8f39f3ff14525c88aebea850a2d71caf9b
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2025-05-22 15:21:02 +0200

    feat: update homepage to display CV as a PDF embed

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
 <template>
-  <div class="m-4 rounded-lg bg-gray-100 p-4">
-    <h1 class="mb-4 text-2xl font-bold">Welcome to the homepage</h1>
-    <div>This is an auto-imported component</div>
-  </div>
+  <embed src="/pdf/cv.pdf" type="application/pdf" class="h-screen w-full" />
 </template>
