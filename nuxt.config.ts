5d33ea9d653a4e2120eb7db35509cc92690572e0
greglmx0 <gregoire.lamoureux@epitech.eu>
2025-06-11 09:17:03 +0200

    feat: update app head configuration and replace favicon with head.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Portfolio - Gregoire Lamoureux',
+      htmlAttrs: {
+        lang: 'fr',
+      },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/head.png' }],
+    },
+  },
 
   modules: [
     '@nuxt/eslint',
